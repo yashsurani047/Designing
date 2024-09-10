20bfa654b4b74be1187e14ec9c7815d46a22a087
yash_surani <yashsurani048@gmail.com>
2024-09-11 01:32:46 +0530

    Student Homepage Changes

--- html/functions/basic.php
+++ html/functions/basic.php
@@ -62,6 +62,17 @@ function headTag($path)
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src='https://buttons.github.io/buttons.js'></script>
 
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJTYo2MsMefpH84eiEFBzHgIhC9H7PRZ8mEcfM/sFJpDYLj5' crossorigin='anonymous'></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-smHYkd38cqh8yg8pP+RZTyk84PzvBvR1zIvIVLDg6TA11UyV8Viw7moJo4Wl30J2' crossorigin='anonymous'></script>
+
+<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ775/zr03cJIHq5qvIF5t47OVeF5XaXIMr' crossorigin='anonymous'>
+
+<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJTYo2MsMefpH84eiEFBzHgIhC9H7PRZ8mEcfM/sFJpDYLj5' crossorigin='anonymous'></script>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-smHYkd38cqh8yg8pP+RZTyk84PzvBvR1zIvIVLDg6TA11UyV8Viw7moJo4Wl30J2' crossorigin='anonymous'></script>
+
+
 </head>
 <style>
         .error {
